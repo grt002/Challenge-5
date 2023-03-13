734f6d86ea4bffc3dd380ce2b7872a064983a1a4
Garrett <grt002@outlook.com>
2023-03-12 19:21:56 -0500

    First draft complete with readme edits

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,20 @@
+$(document).ready(function() {
+
 // Call JQuery to ensure that code isn't run until the browser has finished rendering all the elements in the HTML.
 const dateContainer = $('#currentDay');
 
-$(function displayCurrentDate() {
+  function displayCurrentDate() {
   const now = dayjs();
   const formattedDate = now.format('dddd, MMMM D, YYYY');
   dateContainer.text(formattedDate);
+}
 
   function updateTimeblockStatus() {
     // Get the current time using Day.js
     var currentTime =dayjs().hour();
 
     // Loop through each timeblock
-    $("time-block").each(function () {
+    $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("-")[1]);
 
       // Check if the timeblock is in the past, present, or future
@@ -45,7 +48,16 @@ $(function displayCurrentDate() {
     var description = localStorage.getItem(timeblockID);
 
     if (description !== null) {
-      $(this).children("description").val(description);
+      $(this).children(".description").val(description);
     }
   });
+
+  // Event listener for clicking a timeblock
+  $(".time-block").on("click", function() {
+    // Select description textarea element within the clicked timeblock
+    var descriptionEl = $(this).find(".description");
+
+    // Set the focus on the textarea element
+    descriptionEl.focus();
+  }); 
 });
